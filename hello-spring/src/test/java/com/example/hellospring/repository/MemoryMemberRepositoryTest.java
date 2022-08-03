94ee791d1bf4fbcdf8d8be5b7cd82d47aaaeeb18
kwang-sub <chlrhkdtjq11@naver.com>
2022-08-03 23:10:40 +0900

    김영한님 스프링 입문 강의 시작

---
+++ hello-spring/src/test/java/com/example/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.example.hellospring.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    void save() {
+
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByName() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}

67c6e6dd626f489a2264d69772c54f12c8ed45df
kwang-sub <chlrhkdtjq11@naver.com>
2022-08-10 19:48:26 +0900

    과거 서블릿, jsp조합 학습 및 스프링 프레임워크 필요성...

---
+++ servlet/src/main/java/hello/servlet/domain/member/Member.java
@@ -0,0 +1,20 @@
+package hello.servlet.domain.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+
+    private Long id;
+    private String username;
+    private int age;
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+}

0ff679700b2d4288842cc997ba51b194f46d075a
kwang-sub <chlrhkdtjq11@naver.com>
2022-08-15 22:41:12 +0900

    validation 학습 완료

---
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,29 @@
+package hello.itemservice.domain.item;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity > 10000", message = "총합 10000원 넘게 입력하세요")
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
